e86fc5d761db31d03e7fdcf0b82e01dd64a18cb1
Tiago Facco <thifacco@gmail.com>
2025-03-18 10:39:51 -0300

    Menu de opções

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print ('hello Python!')
+print('Sabor Express\n')
+
+print('1. Cadastrar restaurante')
+print('2. Listar restaurante')
+print('3. Ativar restaurante')
+print('4. Sair\n')
+
+opcao_escolhida = input('Escolha uma opcao: ')
+print('Voce escolheu a opcao ', opcao_escolhida)
