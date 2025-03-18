beba08e0b7332571ba99556b032807db2550dbac
Tiago Facco <thifacco@gmail.com>
2025-03-18 16:04:18 -0300

    Entrada de dado do tipo inteiro

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-print('Sabor Express\n')
+# https://fsymbols.com/pt/geradores/
+print('Ｓａｂｏｒ Ｅｘｐｒｅｓｓ\n')
 
 print('1. Cadastrar restaurante')
 print('2. Listar restaurante')
 print('3. Ativar restaurante')
 print('4. Sair\n')
 
-opcao_escolhida = input('Escolha uma opção: ')
-print('Você escolheu a opção ',opcao_escolhida)
+opcao_escolhida = int(input('Escolha uma opção: '))
+
+if opcao_escolhida == 1:
+    print('Cadastrar restaurante')
+elif opcao_escolhida == 2:
+    print('Listar restaurantes')
+elif opcao_escolhida == 3:
+    print('Ativar restaurantes')
+else:
+    print('Encerrando o programa')
