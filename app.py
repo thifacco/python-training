83da6e09495c5f957a90bc83f4c6f190f9de1056
Tiago Facco <thifacco@gmail.com>
2025-03-19 16:30:00 -0300

    Transformando tudo em funções

--- app.py
+++ app.py
@@ -1,24 +1,35 @@
 import os
 
-# https://fsymbols.com/pt/geradores/
-print('Ｓａｂｏｒ Ｅｘｐｒｅｓｓ\n')
+def exibir_nome_programa():
+   # https://fsymbols.com/pt/geradores/
+   print('Ｓａｂｏｒ Ｅｘｐｒｅｓｓ\n')
 
-print('1. Cadastrar restaurante')
-print('2. Listar restaurante')
-print('3. Ativar restaurante')
-print('4. Sair\n')
-
-opcao_escolhida = int(input('Escolha uma opção: '))
+def exibir_opcoes():
+   print('1. Cadastrar restaurante')
+   print('2. Listar restaurante')
+   print('3. Ativar restaurante')
+   print('4. Sair\n')
 
 def finalizar_app():
     print('Encerrando o programa\n')
     os.system('clear')
 
-if opcao_escolhida == 1:
-    print('Cadastrar restaurante')
-elif opcao_escolhida == 2:
-    print('Listar restaurantes')
-elif opcao_escolhida == 3:
-    print('Ativar restaurantes')
-else:
-    finalizar_app()
+def escolher_opcoes():
+   opcao_escolhida = int(input('Escolha uma opção: '))
+   
+   if opcao_escolhida == 1:
+      print('Cadastrar restaurante')
+   elif opcao_escolhida == 2:
+      print('Listar restaurantes')
+   elif opcao_escolhida == 3:
+      print('Ativar restaurantes')
+   else:
+      finalizar_app()
+
+def main():
+   exibir_nome_programa()
+   exibir_opcoes()
+   escolher_opcoes()
+
+if __name__ == '__main__':
+    main()
