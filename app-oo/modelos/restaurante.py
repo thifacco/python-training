94d418a58d5b1631abae77ba56dbfa987d4174ed
Tiago Facco <thifacco@gmail.com>
2025-03-24 15:14:10 -0300

    Exibe objeto em formato texto

--- app-oo/modelos/restaurante.py
+++ app-oo/modelos/restaurante.py
@@ -3,11 +3,20 @@ class Restaurante:
     self.nome = nome
     self.categoria = categoria
     self.ativo = False
+   
+   def __str__(self):
+    return f"Restaurante {self.nome} | Categoria {self.categoria} | Ativo: {self.ativo}"
     
 restaurante_praca = Restaurante('Restaurante da Praça', 'Prato Feito')
+
+# imprime as propriedades do objeto
+print(dir(restaurante_praca))
 
 # imprime os atributos do objeto
 print(vars(restaurante_praca))
 
 # imprime o valor de um atributo específico
 print(restaurante_praca.nome)
+
+# mostra o objeto em formato texto
+print(restaurante_praca)
