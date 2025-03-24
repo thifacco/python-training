f9153f651bd75650eb6ec164b4a1a03d3596b1a1
Tiago Facco <thifacco@gmail.com>
2025-03-24 15:21:51 -0300

    Métodos especiais X métodos próprios

--- app-oo/modelos/restaurante.py
+++ app-oo/modelos/restaurante.py
@@ -1,11 +1,21 @@
 class Restaurante:
+   Restaurantes = []
+   
+   # método construtor (especial)
    def __init__(self, nome, categoria):
     self.nome = nome
     self.categoria = categoria
     self.ativo = False
+    Restaurante.Restaurantes.append(self)
    
+   # método para exibir o objeto em formato texto (especial)
    def __str__(self):
     return f"Restaurante {self.nome} | Categoria {self.categoria} | Ativo: {self.ativo}"
+   
+   # método para listar o restaurante (próprio)
+   def listar_restaurantes():
+    for restaurante in Restaurante.Restaurantes:
+        print(f'Nome: {restaurante.nome} | Categoria: {restaurante.categoria} | Ativo: {restaurante.ativo}')
     
 restaurante_praca = Restaurante('Restaurante da Praça', 'Prato Feito')
 
@@ -20,3 +30,6 @@ print(restaurante_praca.nome)
 
 # mostra o objeto em formato texto
 print(restaurante_praca)
+
+# chama o método listar_restaurantes
+Restaurante.listar_restaurantes()
