3a2fc102d9b685102148bb9a3df00570bb80207e
Tiago Facco <thifacco@gmail.com>
2025-03-24 16:19:33 -0300

    Método para alternar estado

--- app-oo/modelos/restaurante.py
+++ app-oo/modelos/restaurante.py
@@ -3,26 +3,32 @@ class Restaurante:
    
    # método construtor (especial)
    def __init__(self, nome, categoria):
-    self._nome = nome.title()
-    self._categoria = categoria.upper()
-    self._ativo = False
-    Restaurante.Restaurantes.append(self)
+      self._nome = nome.title()
+      self._categoria = categoria.upper()
+      self._ativo = False
+      Restaurante.Restaurantes.append(self)
    
    # método para exibir o objeto em formato texto (especial)
    def __str__(self):
-    return f"Restaurante {self._nome} | Categoria {self._categoria} | Ativo: {self.ativo}"
+      return f"Restaurante {self._nome} | Categoria {self._categoria} | Ativo: {self.ativo}"
    
    # método para listar o restaurante (próprio)
-   def listar_restaurantes():
-    print(f'{'Nome do Restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | Status')
-    for restaurante in Restaurante.Restaurantes:
-        print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} |  {restaurante.ativo}')
+   @classmethod
+   def listar_restaurantes(cls):
+      print(f'{'Nome do Restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | Status')
+      for restaurante in cls.Restaurantes:
+         print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} |  {restaurante.ativo}')
    
    @property
    def ativo(self):
-    return '✅' if self._ativo == True else '❌'
+      return '✅' if self._ativo == True else '❌'
+
+   def alternar_estado(self):
+      self._ativo = not self._ativo
     
-restaurante_praca = Restaurante('Praça', 'Prato Feito')
+restaurante_praca = Restaurante('Bresser', 'Paulistano')
+restaurante_praca.alternar_estado()
+restaurante_pizza = Restaurante('Pizza Hut', 'Pizzaria')
 
 # imprime as propriedades do objeto
 # print(dir(restaurante_praca))
