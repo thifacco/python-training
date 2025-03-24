b55d4005693c87ad659e13535b10041b7c456cea
Tiago Facco <thifacco@gmail.com>
2025-03-24 10:53:35 -0300

    Exibição de atributos

--- app-oo/modelos/restaurante.py
+++ app-oo/modelos/restaurante.py
@@ -4,8 +4,17 @@ class Restaurante:
     ativo = False
     
 restaurante_praca = Restaurante()
+restaurante_praca.nome = 'Restaurante da Praça'
+restaurante_praca.categoria = 'Prato Feito'
 restaurante_pizza = Restaurante()
 
 restaurantes = [restaurante_praca, restaurante_pizza]
 
-print(restaurantes)
+# imprime o objeto completo
+print(dir(restaurantes))
+
+# imprime os atributos do objeto
+print(vars(restaurante_praca))
+
+# imprime o valor de um atributo específico
+print(restaurante_praca.nome)
