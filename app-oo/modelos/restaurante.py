071d2cd604e1535f112f67de556978948c940382
Tiago Facco <thifacco@gmail.com>
2025-03-24 15:59:28 -0300

    Regra de negócio: definir o nome como maiúsculo

--- app-oo/modelos/restaurante.py
+++ app-oo/modelos/restaurante.py
@@ -3,37 +3,38 @@ class Restaurante:
    
    # método construtor (especial)
    def __init__(self, nome, categoria):
-    self.nome = nome
-    self.categoria = categoria
+    self._nome = nome.title()
+    self._categoria = categoria.upper()
     self._ativo = False
     Restaurante.Restaurantes.append(self)
    
    # método para exibir o objeto em formato texto (especial)
    def __str__(self):
-    return f"Restaurante {self.nome} | Categoria {self.categoria} | Ativo: {self.ativo}"
+    return f"Restaurante {self._nome} | Categoria {self._categoria} | Ativo: {self.ativo}"
    
    # método para listar o restaurante (próprio)
    def listar_restaurantes():
+    print(f'{'Nome do Restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | Status')
     for restaurante in Restaurante.Restaurantes:
-        print(f'Nome: {restaurante.nome} | Categoria: {restaurante.categoria} | Ativo: {restaurante.ativo}')
+        print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} |  {restaurante.ativo}')
    
    @property
    def ativo(self):
     return '✅' if self._ativo == True else '❌'
     
-restaurante_praca = Restaurante('Restaurante da Praça', 'Prato Feito')
+restaurante_praca = Restaurante('Praça', 'Prato Feito')
 
 # imprime as propriedades do objeto
-print(dir(restaurante_praca))
+# print(dir(restaurante_praca))
 
 # imprime os atributos do objeto
-print(vars(restaurante_praca))
+# print(vars(restaurante_praca))
 
 # imprime o valor de um atributo específico
-print(restaurante_praca.nome)
+# print(restaurante_praca._nome)
 
 # mostra o objeto em formato texto
-print(restaurante_praca)
+# print(restaurante_praca)
 
 # chama o método listar_restaurantes
 Restaurante.listar_restaurantes()
