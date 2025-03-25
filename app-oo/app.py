b854dbe6f1f1f1851d13d838a151dfc636506a77
Tiago Facco <thifacco@gmail.com>
2025-03-24 22:43:32 -0300

    Média de avaliações

--- app-oo/app.py
+++ app-oo/app.py
@@ -5,7 +5,7 @@ restaurante_cafe_garden = Restaurante('Café Garden', 'Gourmet')
 restaurante_cafe_garden.alternar_estado()
 restaurante_jacare_madalena = Restaurante('Jacaré Madalena', 'Grill')
 restaurante_jacare_madalena.receber_avaliacao('João', 5)
-restaurante_jacare_madalena.receber_avaliacao('Maria', 4)
+restaurante_jacare_madalena.receber_avaliacao('Maria', 10)
 restaurante_jacare_madalena.receber_avaliacao('José', 3)
 restaurante_jacare_madalena.receber_avaliacao('Ana', 2)
 
--- app-oo/modelos/restaurante.py
+++ app-oo/modelos/restaurante.py
@@ -18,9 +18,9 @@ class Restaurante:
    # método para listar o restaurante (próprio)
    @classmethod
    def listar_restaurantes(cls):
-      print(f'{'Nome do Restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | Status')
+      print(f'{'Nome do Restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Avaliação'.ljust(25)} Status')
       for restaurante in cls.Restaurantes:
-         print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} |  {restaurante.ativo}')
+         print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {str(restaurante.media_avaliacoes).ljust(25)} | {restaurante.ativo}')
    
    @property
    def ativo(self):
@@ -32,3 +32,14 @@ class Restaurante:
    def receber_avaliacao(self, cliente, nota):
       avaliacao = Avaliacao(cliente, nota)
       self._avaliacao.append(avaliacao)
+   
+   @property
+   def media_avaliacoes(self):
+      if not self._avaliacao:
+         return 0
+      
+      soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
+      quantidade_de_notas = len(self._avaliacao)
+      media = round(soma_das_notas / quantidade_de_notas, 1)
+      
+      return media
