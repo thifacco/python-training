56a3ba5c924b48e51124e2a2ec78c69e2eb9c60e
Tiago Facco <thifacco@gmail.com>
2025-03-25 15:35:04 -0300

    Implementação método abstrato  em classes filhas

--- app-restaurante/app.py
+++ app-restaurante/app.py
@@ -3,9 +3,13 @@ from modelos.cardapio.prato import Prato
 from modelos.cardapio.bebida import Bebida
 
 restaurante_marie = Restaurante('Marie', 'Cozinha Mediterrânea')
-bebida_sangria = Bebida('Sangria de Frutas', 35.0, 'médio')
-prato_macarrao = Prato('Macarrão Al Mare', 2.00, 'Massa com frutos do mar')
+
+bebida_sangria = Bebida('Sangria de Frutas', 25.0, 'médio')
+bebida_sangria.aplicar_desconto()
 restaurante_marie.adicionar_cardapio(bebida_sangria)
+
+prato_macarrao = Prato('Pasta Al Mare', 80.00, 'Massa com frutos do mar')
+prato_macarrao.aplicar_desconto()
 restaurante_marie.adicionar_cardapio(prato_macarrao)
 
 def main():
--- app-restaurante/modelos/restaurante.py
+++ app-restaurante/modelos/restaurante.py
@@ -57,8 +57,8 @@ class Restaurante:
       print(f'Cardapio do restaurante {self._nome}\n')
       for i,item in enumerate(self._cardapio,start=1):
          if hasattr(item,'descricao'):
-            mensagem_prato = f'{i}. Nome: {item._nome} | Preço: R${item._preco} | Descrição: {item.descricao}'
+            mensagem_prato = f'{i}. Nome: {item._nome.ljust(20)} | Preço: R${str(item._preco).ljust(10)} | Descrição: {item.descricao}'
             print(mensagem_prato)
          else:
-            mensagem_bebida = f'{i}. Nome: {item._nome} | Preço: R${item._preco} | Tamanho: {item.tamanho}'
+            mensagem_bebida = f'{i}. Nome: {item._nome.ljust(20)} | Preço: R${str(item._preco).ljust(10)} | Tamanho: {item.tamanho}'
             print(mensagem_bebida)
