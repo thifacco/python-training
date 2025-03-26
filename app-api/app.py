c6c51426893115dd226c07de07cceea1e8b8c087
Tiago Facco <thifacco@gmail.com>
2025-03-26 08:40:21 -0300

    Response JSON

--- app-api/app.py
+++ app-api/app.py
@@ -6,6 +6,18 @@ print(response)
 
 if response.status_code == 200:
    dados_json = response.json()
-   print(dados_json)
-else: 
+   dados_restaurante = {}
+   for item in dados_json:
+      nome_do_restaurante = item['Company']
+      if nome_do_restaurante not in dados_restaurante:
+         dados_restaurante[nome_do_restaurante] = []
+      
+      dados_restaurante[nome_do_restaurante].append({
+         "item": item['Item'],
+         "price": item['price'],
+         "description": item['description']
+      })
+   
+   print(dados_restaurante['McDonald’s'])
+else:
    print(f'O erro foi {response.status_code}')
